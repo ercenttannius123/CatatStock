5fbff67be7e48b7d70ad6b00f596acbe33285df9
ErcentTannius <ercenttannius2006@gmail.com>
2026-06-08 23:18:52 +0700

    Update RegisterForm.jsx

--- src/RegisterForm.jsx
+++ src/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://catatstock-production.up.railway.app/';
 
 const RegisterForm = ({ onSwitch, onLogin }) => {
   const [name, setName] = useState('');
